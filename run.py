2f346d43304ae18d31e3d92665c66cb3c6263313
Christy <173209W@mymail.nyp.edu.sg>
2018-01-12 00:55:02 +0800

    Initial commit

---
+++ run.py
@@ -0,0 +1,11 @@
+from flask import Flask, redirect, url_for, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/workout')
+def hello(name):
+    return render_template('home.html')
